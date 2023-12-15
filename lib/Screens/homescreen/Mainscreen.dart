15e9da2cd96b971893cec1f5ebeff147193addab
Midhun NK <midhunnk2021@gmail.com>
2023-12-15 18:45:26 +0530

    MainScreenUpdated

--- lib/Screens/homescreen/Mainscreen.dart
+++ lib/Screens/homescreen/Mainscreen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+
 import 'package:music_app/Screens/basicScreen/ProfileScreen.dart';
 
 class MainScreen extends StatelessWidget {
   // Remove const from the constructor
   const MainScreen({Key? key}) : super(key: key);
+  Widget ListBlock() => Container(
+        width: 200,
+        height: 200,
+        child: Image.asset("assets/image/demo2.jpg"),
+      );
+       Widget ListBlock1() => Container(
+        width: 200,
+        height: 200,
+        child: Image.asset("assets/image/demo1.jpg"),
 
+      );
+       Widget ListBlock2() => Container(
+        width: 200,
+        height: 200,
+        child: Image.asset("assets/image/demo3.jpg"),
+
+      );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +33,7 @@ class MainScreen extends StatelessWidget {
           color: const Color.fromARGB(255, 28, 26, 26),
           child: AppBar(
             backgroundColor: Color.fromARGB(255, 0, 0, 0),
-            title: Padding(
+            title: const Padding(
               padding: EdgeInsets.only(left: 25),
               child: Text(
                 "Mike Studio",
@@ -65,69 +82,159 @@ class MainScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 20),
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "All",
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 201, 255, 216),
-                        fontSize: 15,
-                        fontWeight: FontWeight.w200,
-                      ),
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 20),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "All",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 201, 255, 216),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w200,
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15, top: 20),
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Recommended",
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 201, 255, 216),
-                        fontSize: 15,
-                        fontWeight: FontWeight.w200,
-                      ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, top: 20),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Recommended",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 201, 255, 216),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w200,
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15, top: 20),
-                  child: OutlinedButton(
-                    
-                    onPressed: () {},
-                    child: const Text(
-                      "Podcast",
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 201, 255, 216),
-                        fontSize: 15,
-                        fontWeight: FontWeight.w200,
-                      ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, top: 20),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Podcast",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 201, 255, 216),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w200,
                     ),
                   ),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Center(
-              child: Text(
-                "Home Screen here",
-                style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+         const Padding(
+            padding:  EdgeInsets.only(left: 20),
+            child: Text(
+              "Today Biggest Hits",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
               ),
             ),
-            
-          ],
-        ),
+          ),
+          Container(
+            height: 250,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: ListView.separated(
+                itemCount: 6,
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 10,
+                ),
+                itemBuilder: (context, index) => ListBlock2(),
+                
+                scrollDirection: Axis.horizontal,
+                
+              
+               
+                
+              ),
+            ),
+
+          ),
+           const Padding(
+            padding:  EdgeInsets.only(left: 20),
+            child: Text(
+              "Shows That You Might Like",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+
+          Container(
+            height: 180,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: ListView.separated(
+                itemCount: 6,
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 10,
+                ),
+                itemBuilder: (context, index) => ListBlock1(),
+                
+                scrollDirection: Axis.horizontal,
+                
+              
+               
+                
+              ),
+            ),
+          ),
+           const Padding(
+            padding:  EdgeInsets.only(left: 20),
+            child: Text(
+              "Recently Played Albums",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+
+          Container(
+            height: 180,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: ListView.separated(
+                itemCount: 6,
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 10,
+                ),
+                itemBuilder: (context, index) => ListBlock(),
+                
+                scrollDirection: Axis.horizontal,
+                
+              
+               
+                
+              ),
+            ),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
