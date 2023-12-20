9bb51ef272c628f5f49110529f9dda7779eb6771
AJ-1001 <asimjasim290901@gmail.com>
2023-12-20 17:37:43 +0530

    keyboard error fixed

--- lib/Screens/basicScreen/siginin_screen.dart
+++ lib/Screens/basicScreen/siginin_screen.dart
@@ -4,17 +4,60 @@ import 'package:music_app/components/log_textfield.dart';
 import 'package:music_app/components/log_button.dart';
 import 'package:music_app/components/square_box.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   //controllers
   final emailController = TextEditingController();
+
   final passwordcontroller = TextEditingController();
 
   //signin
   void signUserIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text, password: passwordcontroller.text);
+    //load
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+    //signin
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordcontroller.text);
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+      signinErrorMessage(e.code);
+    }
+
+    //done load
+    Navigator.pop(context);
+  }
+
+  void signinErrorMessage(String m) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: const Color.fromARGB(255, 64, 186, 68),
+          title: Center(
+            child: Text(
+              m,
+              style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -23,154 +66,156 @@ class LoginScreen extends StatelessWidget {
       backgroundColor: Colors.black,
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 49,
-              ),
-              //musicapp logo
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "assets/image/demo6.jpg",
-                    height: 60,
-                    width: 60,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              //Hola Amigo
-              const Text(
-                "Hola Amigo",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 88, 227, 93),
-                  fontSize: 17,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 49,
                 ),
-              ),
-              const SizedBox(
-                height: 23,
-              ),
-
-              //username
-              LogTextField(
-                controller: emailController,
-                hintText: 'Email',
-                obscureText: false,
-              ),
-
-              const SizedBox(
-                height: 10,
-              ),
-
-              //password
-              LogTextField(
-                controller: passwordcontroller,
-                hintText: 'Secret Password',
-                obscureText: true,
-              ),
-
-              const SizedBox(
-                height: 10,
-              ),
-              //forgot
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Forgot Password?',
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 103, 103, 103)),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(
-                height: 25,
-              ),
-              //signin
-              LogButton(
-                onTap: signUserIn,
-              ),
-
-              const SizedBox(
-                height: 40,
-              ),
-
-              //or op
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Color.fromARGB(255, 11, 185, 43),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or continue with',
-                        style:
-                            TextStyle(color: Color.fromARGB(255, 11, 185, 43)),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Color.fromARGB(255, 11, 185, 43),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              //google
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SquareBox(imagepath: 'assets/image/google_old.jpg'),
-                ],
-              ),
-
-              const SizedBox(
-                height: 40,
-              ),
-
-              //signup
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
+                //musicapp logo
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      'Not a member Yet?',
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 103, 103, 103)),
-                    ),
-                    SizedBox(
-                      width: 6,
-                    ),
-                    Text(
-                      'Register Now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    Image.asset(
+                      "assets/image/demo6.jpg",
+                      height: 60,
+                      width: 60,
                     ),
                   ],
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 40,
+                ),
+                //Hola Amigo
+                const Text(
+                  "Hola Amigo",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 88, 227, 93),
+                    fontSize: 17,
+                  ),
+                ),
+                const SizedBox(
+                  height: 23,
+                ),
+
+                //username
+                LogTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                //password
+                LogTextField(
+                  controller: passwordcontroller,
+                  hintText: 'Secret Password',
+                  obscureText: true,
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+                //forgot
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 103, 103, 103)),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+                //signin
+                LogButton(
+                  onTap: signUserIn,
+                ),
+
+                const SizedBox(
+                  height: 40,
+                ),
+
+                //or op
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Color.fromARGB(255, 11, 185, 43),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or continue with',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 11, 185, 43)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Color.fromARGB(255, 11, 185, 43),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                //google
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SquareBox(imagepath: 'assets/image/google_old.jpg'),
+                  ],
+                ),
+
+                const SizedBox(
+                  height: 40,
+                ),
+
+                //signup
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not a member Yet?',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 103, 103, 103)),
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        'Register Now',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
