93b88888924b320926ea2f64be0e0d58fa9d7a91
Midhun NK <midhunnk2021@gmail.com>
2023-12-25 19:40:24 +0530

    play screen list added

--- lib/Screens/homescreen/playscreen.dart
+++ lib/Screens/homescreen/playscreen.dart
@@ -1,144 +1,8 @@
-// import 'package:flutter/material.dart';
-// class SearchScreen extends StatelessWidget {
-//   const SearchScreen({Key? key});
 
-//   @override
-//   Widget build(BuildContext context) {
-
-//     Widget ListBlock4() => Container(
-//           width: 200,
-//           height: 200,
-//           child: Image.asset("assets/image/demo4.jpg"),
-//         );
-//  Widget GrindBlock() => Container(
-//           width: 50,
-//           height: 50,
-//           child: Image.asset("assets/image/demo5.jpg",fit: BoxFit.fill,),
-//         );
-//     return Scaffold(
-//       backgroundColor: Colors.black,
-//       appBar: AppBar(
-//         backgroundColor: Colors.black,
-//         title: const Padding(
-//           padding: EdgeInsets.only(left: 15),
-//           child: Text(
-//             'Search',
-//             style: TextStyle(
-//                 color: Colors.white,
-//                 fontWeight: FontWeight.w500,
-//                 fontSize: 25),
-//           ),
-//         ),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Center(
-//               child: Padding(
-//                 padding: const EdgeInsets.only(top: 5),
-//                 child: Container(
-//                   width: 350,
-//                   height: 50,
-//                   decoration: BoxDecoration(
-//                     color: Colors.grey[300],
-//                     borderRadius: BorderRadius.circular(10),
-//                     border: Border.all(
-//                       color: Colors.grey,
-//                     ),
-//                   ),
-//                   child: const TextField(
-//                       decoration: InputDecoration(
-//                     hintText: "Search for Artists, Songs, Albums",
-//                     hintStyle: TextStyle(
-//                       color: Color.fromARGB(255, 57, 57, 57),
-//                       fontSize: 15,
-//                     ),
-//                     border: InputBorder.none,
-//                     prefixIcon: Icon(
-//                       Icons.search,
-//                       color: Colors.black,
-//                     ),
-//                   )),
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 15,),
-//             const Padding(
-//               padding: EdgeInsets.only(left: 20),
-//               child: Text(
-//                 "Picked For You",
-//                 textAlign: TextAlign.left,
-//                 style: TextStyle(
-//                   color: Color.fromARGB(255, 255, 255, 255),
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w500,
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 10,),
-//             const Padding(
-//               padding: EdgeInsets.only(left: 20),
-//               child: Text(
-//                 "Browse All",
-//                 textAlign: TextAlign.left,
-//                 style: TextStyle(
-//                   color: Color.fromARGB(255, 255, 255, 255),
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w500,
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 1,),
-//             Container(
-//               height: 180,
-//               child: Padding(
-//                 padding: const EdgeInsets.only(left: 10),
-//                 child: ListView.separated(
-//                   itemCount: 6,
-//                   separatorBuilder: (context, index) => const SizedBox(
-//                     width: 10,
-//                   ),
-//                   itemBuilder: (context, index) => ListBlock4(),
-//                   scrollDirection: Axis.horizontal,
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 20,),
-//             const Padding(
-//               padding: EdgeInsets.only(left: 20),
-//               child: Text(
-//                 "Grid View",
-//                 textAlign: TextAlign.left,
-//                 style: TextStyle(
-//                   color: Color.fromARGB(255, 255, 255, 255),
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w500,
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 10,),
-//             GridView.builder(
-//               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                 crossAxisCount: 2,
-//                 crossAxisSpacing: 10.0,
-//                 mainAxisSpacing: 10.0,
-//               ),
-//               itemCount: 6,
-//               shrinkWrap: true,
-//               physics: NeverScrollableScrollPhysics(),
-//               itemBuilder: (context, index) => GrindBlock(),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:music_app/Screens/homescreen/Playlistscreen.dart';
 
 class MusicPlayer extends StatefulWidget {
   const MusicPlayer({Key? key}) : super(key: key);
@@ -160,11 +24,56 @@ class _MusicPlayState extends State<MusicPlayer> {
   Duration _totalDuration = Duration();
   Duration _currentPosition = Duration();
 
+
+
+  late List<Map<String, dynamic>> items = [];
+  bool isLoaded = false;
+
+
+  _incrementCounter() async {
+    try {
+      List<Map<String, dynamic>> tempList = [];
+      QuerySnapshot data = await firestore.collection("SongDetails").get();
+
+      print("Number of documents: ${data.docs.length}");
+
+      data.docs.forEach((element) {
+        tempList.add(element.data() as Map<String, dynamic>);
+      });
+
+      setState(() {
+        items = tempList;
+        isLoaded = true;
+      });
+    } catch (e) {
+      print("Error fetching data: $e");
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   @override
   void initState() {
     super.initState();
     _audioPlayer = AudioPlayer();
-
+    _incrementCounter();
+    _audioPlayer.onPlayerComplete.listen((event) {
+      playNext();
+    });
     _audioPlayer.onDurationChanged.listen((Duration duration) {
       setState(() {
         _totalDuration = duration;
@@ -190,13 +99,12 @@ class _MusicPlayState extends State<MusicPlayer> {
     _audioPlayer.dispose();
     super.dispose();
   }
-
+int index1=0;
   Future<void> play() async {
-    final url =
-        "https://firebasestorage.googleapis.com/v0/b/musicapp-156cc.appspot.com/o/Mp3s%2FOrdinary%20Person%20(From%20_Leo_).mp3.mp3?alt=media&token=c641ce36-9959-4026-8680-769953e9d260";
-
+    final url = items[index1]["songUrl"];
     if (_audioPlayer.state == PlayerState.playing) {
-      _audioPlayer.pause();
+      await _audioPlayer.pause();
+      
     } else {
       await _audioPlayer.play(UrlSource(url));
     }
@@ -214,6 +122,22 @@ class _MusicPlayState extends State<MusicPlayer> {
     Duration newPosition = Duration(seconds: seconds.toInt());
     _audioPlayer.seek(newPosition);
   }
+void playNext() {
+  if (index1 < items.length - 1) {
+    index1++;
+    play();
+  }
+}
+void playPrevious() {
+  if (index1 > 0) {
+    index1--;
+    play();
+  }
+}
+
+
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -412,7 +336,7 @@ class _MusicPlayState extends State<MusicPlayer> {
                   ),
                 ),
                 IconButton(
-                  onPressed: () {},
+                  onPressed: playPrevious,
                   icon: const Icon(
                     Icons.skip_previous,
                     color: Colors.white,
@@ -448,7 +372,7 @@ class _MusicPlayState extends State<MusicPlayer> {
                   ),
                 ),
                 IconButton(
-                  onPressed: () {},
+                  onPressed: playNext,
                   icon: const Icon(
                     Icons.skip_next,
                     color: Colors.white,
